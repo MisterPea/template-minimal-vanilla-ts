3ab57c927c374a61af1d40a4da30c298d4c82372
Perry Angelora <pea.mail.list@gmail.com>
2024-12-03 22:34:20 -0500

    Added mini-css-extract - bumped new package versions

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,52 @@
-const path = require( "path" );
-const HtmlWebpackPlugin = require( "html-webpack-plugin" );
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: "./src/index.ts",
-  output: {
-    path: path.resolve( __dirname, "dist" ),
-    filename: "bundle.js"
-  },
-  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
-  module: {
-    rules: [
-      {
-        test: /\.csv$/,
-        loader: 'csv-loader',
-        options: {
-          dynamicTyping: true,
-          header: true,
-          skipEmptyLines: true
-        }
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: ['style-loader', 'css-loader', 'sass-loader']
-      },
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ]
-  },
-  devServer: {
-    client: {
-      webSocketTransport: 'sockjs', // Needed b/c webSocket was crashing devServer
+    entry: "./src/index.ts",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+        clean: true,
     },
-    webSocketServer: 'sockjs',
-    static: './dist',
-    hot: true,
-    historyApiFallback: true,
-    port: 8080,
-    host: '192.168.1.xxx',
-  },
-  plugins: [
-    new HtmlWebpackPlugin( { template: './src/index.html' } )
-  ]
+    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+    module: {
+        rules: [
+            {
+                test: /\.csv$/,
+                loader: 'csv-loader',
+                options: {
+                    dynamicTyping: true,
+                    header: true,
+                    skipEmptyLines: true
+                }
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ]
+    },
+    devServer: {
+        client: {
+            webSocketTransport: 'sockjs', // Needed b/c webSocket was crashing devServer
+        },
+        webSocketServer: 'sockjs',
+        static: './dist',
+        hot: true,
+        historyApiFallback: true,
+        port: 8080,
+        host: '192.168.1.xxx',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({template: './src/index.html'}),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+        })
+    ]
 };
