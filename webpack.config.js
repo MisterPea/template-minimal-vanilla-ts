5afcdbad62dc47e25b228910dcc943d7fd3dd5a4
Perry Angelora <pea.mail.list@gmail.com>
2024-12-04 10:09:08 -0500

    Fixed build call - added dedicated folders for build

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require( "path" );
+const HtmlWebpackPlugin = require( "html-webpack-plugin" );
+const MiniCssExtractPlugin = require( "mini-css-extract-plugin" );
 
 module.exports = {
     entry: "./src/index.ts",
     output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "bundle.js",
+        path: path.resolve( __dirname, "dist" ),
+        filename: "js/[name].js",
         clean: true,
     },
     mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
@@ -44,9 +44,12 @@ module.exports = {
         host: '192.168.1.xxx',
     },
     plugins: [
-        new HtmlWebpackPlugin({template: './src/index.html'}),
-        new MiniCssExtractPlugin({
-            filename: "[name].css",
-        })
-    ]
+        new HtmlWebpackPlugin( { template: './src/index.html' } ),
+        new MiniCssExtractPlugin( {
+            filename: "style/[name].css",
+        } )
+    ],
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"]
+    }
 };
